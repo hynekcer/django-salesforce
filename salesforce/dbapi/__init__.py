37732d5a40576ddaf428a53092217b07b8ce71d5
Hynek Cernoch <hynek@sdb.cz>
2016-02-10 00:08:54 +0100

    Added an empty DB API module

---
+++ salesforce/dbapi/__init__.py
@@ -0,0 +1,10 @@
+"""
+Python DB API 2.0 - A layer that is independent on Django.
+
+Basic SQL and a small part of PEP 0249 will implemented.
+On the other side, transactions will be never implemented.
+
+Purpose:
+Many Salesforce APIs and low level features need to be well tested with every
+SFDC version, but not rewritten at all with a new Django version.
+"""
