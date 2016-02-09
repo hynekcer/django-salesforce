55cbf9db3d0d7f06efbc2e1b4136a7528c64620f
Hynek Cernoch <hynek@sdb.cz>
2016-02-10 00:09:54 +0100

    Implemented conversion 15 to 18 char - Id case_safe_id

--- salesforce/dbapi/__init__.py
+++ salesforce/dbapi/__init__.py
@@ -2,9 +2,9 @@
 Python DB API 2.0 - A layer that is independent on Django.
 
 Basic SQL and a small part of PEP 0249 will implemented.
-On the other side, transactions will be never implemented.
+On the contrary, transactions will be never implemented.
 
 Purpose:
-Many Salesforce APIs and low level features need to be well tested with every
-SFDC version, but not rewritten at all with a new Django version.
+Many Salesforce APIs and low level features need not be updated and
+tested for every Django version, but can depend on some SFDC version.
 """
