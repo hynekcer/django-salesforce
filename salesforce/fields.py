d48c8ca535b5c56b7f70f1b2b1abf216a7ae6442
Hynek Cernoch <hynek@sdb.cz>
2021-06-10 20:45:04 +0200

    More field types; fix lint

--- salesforce/fields.py
+++ salesforce/fields.py
@@ -208,7 +208,9 @@ class SfField(models.Field):
 
 class CharField(SfField, models.CharField):
     """CharField with sf_read_only attribute for Salesforce."""
-    pass
+
+    def db_type(self, connection: Any) -> str:
+        return 'Text' if not self.choices else 'Picklist'
 
 
 class EmailField(SfField, models.EmailField):
