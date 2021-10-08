0a01f2bfd228d2457ac6b127bce29be66b235bfb
Hynek Cernoch <hynek@sdb.cz>
2021-10-08 12:07:28 +0200

    Prepare migrate management with a --sf-interactive option

---
+++ salesforce/management/commands/migrate.py
@@ -0,0 +1,23 @@
+from django.core.management.commands.migrate import Command as MigrateCommand  # type: ignore[import]
+from django.db import connections
+from salesforce.backend import enterprise
+
+
+class Command(MigrateCommand):
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            '--sf-interactive', action='store_true',
+            help='Run migrate subcommands interactive.',
+        )
+
+    def handle(self, *args, **options):
+        database = options['database']
+        connection = connections[database]
+        if connection.vendor == 'salesforce':
+            connection.migrate_options = {
+                'sf_interactive': options['sf_interactive'],
+            }
+            enterprise.check_enterprise_license()
+        super().handle(*args, **options)
