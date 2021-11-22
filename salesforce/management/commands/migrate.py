94bf725c60aeb91b983b2ecffd8d6a8394ea5ed7
Hynek Cernoch <hynek@sdb.cz>
2021-11-22 16:53:52 +0100

    Migrations can be enabled by: migrate --sf-create-permission-set

--- salesforce/management/commands/migrate.py
+++ salesforce/management/commands/migrate.py
@@ -1,3 +1,4 @@
+from django.core.management.base import CommandError
 from django.core.management.commands.migrate import Command as MigrateCommand  # type: ignore[import]
 from django.db import connections
 from salesforce.backend import enterprise
@@ -15,6 +16,10 @@ class Command(MigrateCommand):
             '--sf-no-check-permissions', action='store_true',
             help='Run migrate without check permissions of CustomObjects.',
         )
+        parser.add_argument(
+            '--sf-create-permission-set', action='store_true',
+            help='only Create PermissionSet "Django_Salesforce" on a new SF database to enable migrations.',
+        )
 
     def handle(self, *args, **options):
         database = options['database']
@@ -25,4 +30,11 @@ class Command(MigrateCommand):
                 'sf_no_check_permissions': options['sf_no_check_permissions'],
             }
             enterprise.check_enterprise_license()
-        super().handle(*args, **options)
+        if options['sf_create_permission_set']:
+            if connection.vendor == 'salesforce':
+                from salesforce.backend.schema import DatabaseSchemaEditor  # pylint:disable=import-outside-toplevel
+                DatabaseSchemaEditor.create_permission_set(connection)
+            else:
+                raise CommandError("The option --sf-create-permission-set requires a Salesforce database")
+        else:
+            super().handle(*args, **options)
